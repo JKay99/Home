ae75eafcf589aeea3b6cc2aa631961d18551b435
JKay99 <157763944+JKay99@users.noreply.github.com>
2024-03-28 17:39:20 -0400

    thursday

---
+++ control/scripts.js
@@ -0,0 +1,31 @@
+$(".BigBox1").draggable({
+	containment: ".walls"
+});
+
+$(".BigBox2").draggable({
+	containment: ".walls"
+});
+
+$(".BigBox3").draggable({
+	containment: ".walls"
+});
+
+$(".BigBox4").draggable({
+	containment: ".walls"
+});
+
+$(".BigBox5").draggable({
+	containment: ".walls"
+});
+
+$(".BigBox6").draggable({
+	containment: ".walls"
+});
+
+$(".BigBox7").draggable({
+	containment: ".walls"
+});
+
+$(".BigBox8").draggable({
+	containment: ".walls"
+});
